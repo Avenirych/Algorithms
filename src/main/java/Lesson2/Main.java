cc4a894e6686158e680d38058e883670fe575623
Sergey <sergeileonov@inbox.ru>
2023-07-09 21:43:17 +0100

    L2

---
+++ src/main/java/Lesson2/Main.java
@@ -0,0 +1,12 @@
+package Lesson2;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[] {5,6,1,3,8,0,9};
+        HeapSort.sort(array); // Используем статический способ доступа
+        System.out.println(Arrays.toString(array));
+
+    }
+}
